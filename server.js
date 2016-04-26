63d8c522b2e7ff4078b6b8857f258c00656335fc
achinkulshrestha <arcaneak@email.com>
2016-04-25 23:33:27 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname+'/src')).listen(process.env.PORT);
+var port = process.env.PORT || 8080;
+connect().use(serveStatic(__dirname+'/src')).listen(port);
